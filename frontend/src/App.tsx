d6b1257b40e12f3fe59c0fba73529ce361acb3fb
andreza <andreza.21pessoa@gmail.com>
2022-03-13 13:54:04 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import Navbar from "components/Navbar";
+import Listing from "components/pages/Listing";
+import Form from "components/pages/Form";
 
 function App() {
   return (
-    <Navbar></Navbar>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
